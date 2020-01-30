3b3907f7c9576f392974d8cc1108dc32cdae85d4
unknown <svclennan@icloud.com>
2020-01-30 13:27:04 -0600

    init

---
+++ BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinaryTree
+    {
+        Node root;
+        public BinaryTree()
+        {
+            
+        }
+        public void AddValue(int data)
+        {
+            Node node = new Node(data);
+            if (root == null)
+            {
+                root = node;
+            }
+            else
+            {
+                Node current = root;
+                if (data < current.data)
+                {
+                    while (data < current.data)
+                    {
+                        while (!(current.leftChild == null))
+                        {
+                            current = current.leftChild;
+                        }
+                        current.leftChild = node;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+        public int data;
+        public Node leftChild;
+        public Node rightChild;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
