e55fce9c6a4806760066347eba1640166ce06a3a
unknown <svclennan@icloud.com>
2020-01-30 14:29:10 -0600

    Created an add and search method. Search method returns a boolean. If the value is found in the tree, it returns true. False otherwise. The add method recursively searches through the tree until the input data is placed to the left or right of a previous node.

--- BinarySearchTree/BinaryTree.cs
+++ BinarySearchTree/BinaryTree.cs
@@ -8,32 +8,135 @@ namespace BinarySearchTree
 {
     class BinaryTree
     {
-        Node root;
+        public Node root;
+        public List<int> values;
         public BinaryTree()
         {
-            
+            values = new List<int>();
         }
-        public void AddValue(int data)
+        private bool AddValue(int data, Node start)
         {
-            Node node = new Node(data);
             if (root == null)
             {
-                root = node;
+                root = new Node(data);
+                values.Add(data);
+                return true;
             }
             else
             {
-                Node current = root;
-                if (data < current.data)
+                if (data <= start.data)
                 {
-                    while (data < current.data)
+                    if (!(start.leftChild == null))
                     {
-                        while (!(current.leftChild == null))
-                        {
-                            current = current.leftChild;
-                        }
-                        current.leftChild = node;
+                        return AddValue(data, start.leftChild);
+                    }
+                    else
+                    {
+                        start.leftChild = new Node(data);
+                        values.Add(data);
+                        return true;
                     }
                 }
+                else if (data > start.data)
+                {
+                    if (!(start.rightChild == null))
+                    {
+                        return AddValue(data, start.rightChild);
+                    }
+                    else
+                    {
+                        start.rightChild = new Node(data);
+                        values.Add(data);
+                        return true;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        public bool AddValue(int data)
+        {
+            if (root == null)
+            {
+                Node node = new Node(data);
+                root = node;
+                values.Add(data);
+                return true;
+            }
+            else
+            {
+                return AddValue(data, root);
+            }
+        }
+        public bool Search(int data)
+        {
+            if (root.data == data)
+            {
+                return true;
+            }
+            else if (data > root.data)
+            {
+                if (root.rightChild == null)
+                {
+                    return false;
+                }
+                return Search(data, root.rightChild);
+            }
+            else if (data < root.data)
+            {
+                if (root.leftChild == null)
+                {
+                    return false;
+                }
+                return Search(data, root.leftChild);
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private bool Search(int data, Node start)
+        {
+            if (start.data == data)
+            {
+                return true;
+            }
+            else if (data > start.data)
+            {
+                if (start.rightChild == null)
+                {
+                    return false;
+                }
+                return Search(data, start.rightChild);
+            }
+            else if (data < start.data)
+            {
+                if (start.leftChild == null)
+                {
+                    return false;
+                }
+                return Search(data, start.leftChild);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Print(Node start)
+        {
+            Console.WriteLine(start.data);
+            if (start.leftChild != null)
+            {
+                Console.WriteLine("/");
+                Print(start.leftChild);
+            }
+            if (start.rightChild != null)
+            {
+                Console.WriteLine("\\");
+                Print(start.rightChild);
             }
         }
     }
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -10,7 +10,16 @@ namespace BinarySearchTree
     {
         static void Main(string[] args)
         {
-
+            BinaryTree binaryTree = new BinaryTree();
+            binaryTree.AddValue(5);
+            binaryTree.AddValue(10);
+            binaryTree.AddValue(3);
+            binaryTree.AddValue(4);
+            binaryTree.AddValue(8);
+            binaryTree.Print(binaryTree.root);
+            Console.WriteLine(binaryTree.Search(3));
+            Console.WriteLine(binaryTree.Search(15));
+            Console.ReadLine();
         }
     }
 }
